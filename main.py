1be23526a8bdcb054ce52b4ddb5efe74138c18fa
Rukshan <arkruka@gmail.com>
2024-02-17 07:28:07 +0100

    feat: :tada: initial commit

---
+++ main.py
@@ -0,0 +1,343 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import time
+
+
+# create empty dataframe with column names type, name, label, hint, constraint, constraint_message, required, default, relevant, calculation, choice_filter, appearance and media
+df = pd.DataFrame(
+    columns=[
+        "type",
+        "name",
+        "label",
+        "hint",
+        "constraint",
+        "constraint_message",
+        "required",
+        "default",
+        "relevant",
+        "calculation",
+        "choice_filter",
+        "appearance",
+        "media",
+        "read_only",
+        "node_set",
+    ]
+)
+
+df_choice = pd.DataFrame(
+    columns=["list_name", "name", "label", "value", "media", "read_only"]
+)
+
+# read the xml file and populate variable
+xml = ""
+with open("./input/cht.xml", "r") as file:
+    xml = file.read()
+
+# parse the xml file
+soup = BeautifulSoup(xml, "xml")
+
+globe_parents = []
+
+
+def get_label_value(text_id):
+    labels = soup.find("itext").find("translation", lang="en").find("text", id=text_id)
+    if labels is None:
+        return None
+    text_value = labels.find("value", form="markdown")
+    if text_value is not None:
+        return text_value.get_text()
+    else:
+        text_value = labels.find("value")
+        return text_value.get_text()
+
+
+def build_form_structure():
+    global df
+    # find the form structure
+    formStructure = soup.find(
+        "data",
+        xmlns="http://openrosa.org/formdesigner/466B8B00-76BA-482F-A396-35BD6F0767DB",
+    )
+
+    return formStructure
+
+
+def traverse(t, current_path=None):
+    global df
+    global globe_parents
+    parents = []
+
+    if current_path is None:
+        current_path = [t.name]
+
+    for tag in t.find_all(recursive=False):
+        if not tag.find():
+            node_type = None
+            # print(" -> ".join(current_path + [tag.name]))
+            # print(tag.name)
+            if current_path[-1] not in parents:
+                if len(parents) > 0:
+                    node_name = parents[-1], "_", current_path[-1]
+                else:
+                    node_name = current_path[-1]
+
+                parents.append(current_path[-1])
+                globe_parents.append(current_path[-1])
+                node_type = "begin_group"
+                df = df._append(
+                    {
+                        "type": node_type,
+                        "name": node_name,
+                        "label": tag.name,
+                        "hint": "",
+                        "constraint": "",
+                        "constraint_message": "",
+                        "required": "",
+                        "default": "",
+                        "relevant": "",
+                        "calculation": "",
+                        "choice_filter": "",
+                        "appearance": "",
+                        "media": "",
+                        "read_only": "",
+                        "node_set": "",
+                    },
+                    ignore_index=True,
+                )
+
+            df = df._append(
+                {
+                    "type": None,
+                    "name": "_".join(current_path + [tag.name]),
+                    "label": tag.name,
+                    "hint": "",
+                    "constraint": "",
+                    "constraint_message": "",
+                    "required": "",
+                    "default": "",
+                    "relevant": "",
+                    "calculation": "",
+                    "choice_filter": "",
+                    "appearance": "",
+                    "media": "",
+                    "read_only": "",
+                    "node_set": "",
+                },
+                ignore_index=True,
+            )
+
+            if (tag == t.find_all(recursive=False)[-1]) and (
+                current_path[-1] in parents
+            ):
+                # print("end_group", current_path[-1])
+                parents.remove(current_path[-1])
+                globe_parents.remove(current_path[-1])
+        else:
+            traverse(tag, current_path + [tag.name])
+            df = df._append(
+                {
+                    "type": "end_group",
+                    "name": tag.name,
+                    "label": tag.name,
+                    "hint": "",
+                    "constraint": "",
+                    "constraint_message": "",
+                    "required": "",
+                    "default": "",
+                    "relevant": "",
+                    "calculation": "",
+                    "choice_filter": "",
+                    "appearance": "",
+                    "media": "",
+                    "read_only": "",
+                    "node_set": "",
+                },
+                ignore_index=True,
+            )
+    # print(globe_parents)
+
+
+# nodes = build_form_structure()
+# traverse(nodes)
+# print("dataframe generated")
+
+
+def parse_binds():
+    global df
+    binds = soup.find_all("bind")
+    for bind in binds:
+        nodeset = bind.get("nodeset")[1:]
+        nodeset = "_".join(nodeset.split("/"))
+        # print(nodeset)
+        df.loc[df["name"] == nodeset, "nodeset"] = nodeset
+        df.loc[df["name"] == nodeset, "required"] = bind.get("required")
+        bind_relevant = bind.get("relevant")
+
+        # map relevant
+        if bind_relevant is not None:
+            bind_relevant_regex = re.findall(r"\/[^\s=]+", bind_relevant)
+            for reg in bind_relevant_regex:
+                reg = reg.strip()
+                if reg.endswith(",") or reg.endswith(")"):
+                    reg_filter = reg[1:-1]
+                    reg_to_name = "_".join(reg_filter.split("/"))
+                else:
+                    reg_filter = reg[1:]
+                    reg_to_name = "_".join(reg_filter.split("/"))
+                bind_relevant = bind_relevant.replace(reg, "${" + reg_to_name + "}")
+            df.loc[df["name"] == nodeset, "relevant"] = bind_relevant
+
+        # map constraints
+        bind_constratint = bind.get("constraint")
+        if bind_constratint is not None:
+            bind_constratint_regex = re.findall(r"\/[^\s=]+", bind_constratint)
+            for reg in bind_constratint_regex:
+                reg = reg.strip()
+                if reg.endswith(",") or reg.endswith(")"):
+                    reg_filter = reg[1:-1]
+                    reg_to_name = "_".join(reg_filter.split("/"))
+                else:
+                    reg_filter = reg[1:]
+                    reg_to_name = "_".join(reg_filter.split("/"))
+                bind_constratint = bind_constratint.replace(
+                    reg, "${" + reg_to_name + "}"
+                )
+            df.loc[df["name"] == nodeset, "constraint"] = bind_constratint
+
+        # map calculations
+        bind_calculate = bind.get("calculate")
+        if bind_calculate is not None:
+            bind_calculate_regex = re.findall(r"\/[^\s=]+", bind_calculate)
+            for reg in bind_calculate_regex:
+                reg = reg.strip()
+                if reg.endswith(",") or reg.endswith(")"):
+                    reg_filter = reg[1:-1]
+                    reg_to_name = "_".join(reg_filter.split("/"))
+                else:
+                    reg_filter = reg[1:]
+                    reg_to_name = "_".join(reg_filter.split("/"))
+                bind_calculate = bind_calculate.replace(reg, "${" + reg_to_name + "}")
+            df.loc[df["name"] == nodeset, "calculation"] = bind_calculate
+
+        # map constraintMsg
+        constraint_message = bind.get("jr:constraintMsg")
+        if constraint_message is not None:
+            constraint_message = constraint_message.split("'")[1]
+            constraint_message = get_label_value(constraint_message)
+            df.loc[df["name"] == nodeset, "constraint_message"] = constraint_message
+
+        # map requiredMsg
+        required_message = bind.get("jr:requiredMsg")
+        if required_message is not None:
+            required_message = required_message.split("'")[1]
+            required_message = get_label_value(required_message)
+            print(required_message)
+
+        # map types
+        bind_type = bind.get("type")
+        if bind_type is not None and "string" in bind_type:
+            df.loc[df["name"] == nodeset, "type"] = "string"
+        if bind_type is not None and "int" in bind_type:
+            df.loc[df["name"] == nodeset, "type"] = "integer"
+        if bind_type is not None and "double" in bind_type:
+            df.loc[df["name"] == nodeset, "type"] = "decimal"
+
+
+def parse_body():
+    global df
+    global df_choice
+    body = soup.find("h:body")
+
+    for tag in body.find_all(recursive=True):
+        tag_ref = tag.get("ref")
+        tag_name = tag.name
+        tag_appearance = tag.get("appearance")
+        tag_ref_to_name = None
+        tag_name_regex = None
+        df_name = None
+
+        if tag_ref is None:
+            continue
+
+        tag_ref_regex = re.findall(r"\/[^\s=':)]+", tag_ref)
+        for reg in tag_ref_regex:
+            tag_ref_to_name = "_".join(reg.split("/")[1:])
+
+        if tag_name == "select1":
+            df.loc[df["name"] == tag_ref_to_name, "type"] = "select_one"
+            for child in tag.find_all("item"):
+                child_label = child.find("label").get("ref")
+                child_label = child_label.split("'")[1]
+                df_element_name = child_label.split("/")[-1].split(":")[0]
+                label_value = get_label_value(child_label)
+
+                value = child.find("value").get_text()
+                df_choice = df_choice._append(
+                    {
+                        "list_name": tag_ref_to_name,
+                        "name": df_element_name,
+                        "label": label_value,
+                        "value": value,
+                    },
+                    ignore_index=True,
+                )
+
+        elif tag_name == "select":
+            df.loc[df["name"] == tag_ref_to_name, "type"] = "select_multiple"
+            for child in tag.find_all("item"):
+                child_label = child.find("label").get("ref")
+                child_label = child_label.split("'")[1]
+                df_element_name = child_label.split("/")[-1].split(":")[0]
+                label_value = get_label_value(child_label)
+
+                value = child.find("value").get_text()
+                df_choice = df_choice._append(
+                    {
+                        "list_name": tag_ref_to_name,
+                        "name": df_element_name,
+                        "label": label_value,
+                        "value": value,
+                    },
+                    ignore_index=True,
+                )
+        # df.loc[df["name"] == tag_ref_to_name, "type"] = odk_type
+
+        if tag_appearance == "field-list":
+            df_name = tag_ref.split("/")[-1]
+            df.loc[df["name"] == df_name, "appearance"] = "field-list"
+
+        elif tag_appearance is not None:
+            df.loc[df["name"] == tag_ref_to_name, "appearance"] = tag_appearance
+
+
+def fill_labels():
+    global df
+    labels = soup.find("itext").find_all("translation", lang="en")
+
+    for label in labels:
+        for text in label.find_all("text"):
+            text_id = text.get("id")
+            text_id_regex = re.findall(r"\/[^\s=':)]+", text_id)
+            text_id_to_name = "_".join(text_id_regex[0].split("/")[1:])
+
+            text_value = text.find("value", form="markdown")
+            if text_value is not None:
+                df.loc[df["name"] == text_id_to_name, "label"] = text_value.get_text()
+            else:
+                text_value = text.find("value")
+                df.loc[df["name"] == text_id_to_name, "label"] = text_value.get_text()
+
+
+print(df.loc[df["name"].isNull()])
+
+parseForm = build_form_structure()
+print("form structure generated")
+traverse(parseForm)
+print("form structure traversed")
+parse_binds()
+print("binds parsed")
+parse_body()
+print("body parsed")
+fill_labels()
+print("labels filled")
